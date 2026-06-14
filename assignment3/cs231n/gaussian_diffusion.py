960bd7088f75fc8cdf4c6e313668759c52f7c04d
jianchao-li <leesuperman2012@gmail.com>
2026-06-14 13:23:28 +0200

    assignment3: ddpm

--- assignment3/cs231n/gaussian_diffusion.py
+++ assignment3/cs231n/gaussian_diffusion.py
@@ -102,7 +102,9 @@ class GaussianDiffusion(nn.Module):
         # Transform x_t and noise to get x_start according to Eq.(4) and Eq.(14).
         # Look at the coeffs in `__init__` method and use the `extract` function.
         ####################################################################
-
+        c1 = extract(self.sqrt_alphas_cumprod, t, x_t.shape)
+        c2 = extract(self.sqrt_one_minus_alphas_cumprod, t, x_t.shape)
+        x_start = (x_t - c2 * noise) / c1
         ####################################################################
         return x_start
 
@@ -121,7 +123,9 @@ class GaussianDiffusion(nn.Module):
         # Transform x_t and noise to get x_start according to Eq.(4) and Eq.(14).
         # Look at the coeffs in `__init__` method and use the `extract` function.
         ####################################################################
-
+        c1 = extract(self.sqrt_alphas_cumprod, t, x_t.shape)
+        c2 = extract(self.sqrt_one_minus_alphas_cumprod, t, x_t.shape)
+        pred_noise = (x_t - c1 * x_start) / c2
         ####################################################################
         return pred_noise
 
@@ -172,7 +176,17 @@ class GaussianDiffusion(nn.Module):
         #   4. Get the mean and std for q(x_{t-1} | x_t, x_0) using self.q_posterior,
         #      and sample x_{t-1}.
         ##################################################################
-        
+        pred = self.model(x_t, t, model_kwargs)
+        if self.objective == "pred_noise":
+          noise = pred
+          x_start = self.predict_start_from_noise(x_t, t, noise)
+        else:
+          x_start = pred
+          noise = self.predict_noise_from_start(x_t, t, x_start)
+        x_start = x_start.clamp(min=-1, max=1)
+        posterior_mean, posterior_std = self.q_posterior(x_start, x_t, t)
+        z = torch.randn_like(x_t)
+        x_tm1 = posterior_std * z + posterior_mean
         ##################################################################
 
         return x_tm1
@@ -217,7 +231,9 @@ class GaussianDiffusion(nn.Module):
         # can be done as: x_t = mu + sigma * noise where noise is sampled from N(0, 1).
         # Approximately 3 lines of code.
         ####################################################################
-
+        c1 = extract(self.sqrt_alphas_cumprod, t, x_start.shape)
+        c2 = extract(self.sqrt_one_minus_alphas_cumprod, t, x_start.shape)
+        x_t = c1 * x_start + c2 * noise
         ####################################################################
         return x_t
 
@@ -238,7 +254,11 @@ class GaussianDiffusion(nn.Module):
         # Finally, compute the weighted MSE loss.
         # Approximately 3-4 lines of code.
         ####################################################################
-
+        x_t = self.q_sample(x_start, t, noise)
+        pred = self.model(x_t, t, model_kwargs)
+        loss = nn.MSELoss(reduction="none")(pred, target)
+        loss *= loss_weight
+        loss = loss.mean()
         ####################################################################
 
         return loss
--- assignment3/cs231n/unet.py
+++ assignment3/cs231n/unet.py
@@ -180,7 +180,13 @@ class Unet(nn.Module):
             # Make sure to exactly follow this structure of ModuleList in order to
             # load a pretrained checkpoint.
             ##################################################################
-
+            down_block = nn.ModuleList(
+              [
+                ResnetBlock(dim_in, dim_in, context_dim),
+                ResnetBlock(dim_in, dim_in, context_dim),
+                Downsample(dim_in, dim_out),
+              ]
+            )
             ##################################################################
             self.downs.append(down_block)
 
@@ -204,7 +210,13 @@ class Unet(nn.Module):
             # Don't forget to account for the skip connections by having 2 x dim_out
             # channels at the input of both ResnetBlocks.
             ##################################################################
-
+            up_block = nn.ModuleList(
+              [
+                Upsample(dim_in, dim_out),
+                ResnetBlock(2 * dim_out, dim_out, context_dim),
+                ResnetBlock(2 * dim_out, dim_out, context_dim),
+              ]
+            )
             self.ups.append(up_block)
             ##################################################################
 
@@ -226,7 +238,9 @@ class Unet(nn.Module):
         # You will have to call self.forward two times.
         # For unconditional sampling, pass None in`text_emb`.
         ##################################################################
-
+        cond = self.forward(x, time, model_kwargs)
+        uncond = self.forward(x, time, model_kwargs={"text_emb": None})
+        x = (1 + cfg_scale) * cond - cfg_scale * uncond
         ##################################################################
 
         return x
@@ -281,7 +295,25 @@ class Unet(nn.Module):
         #      skip connection from the downsampling path.
         #    - Make sure to pass the context to each ResNet block.
         ##################################################################
-
+        skips = []
+        for down_block in self.downs:
+          for module in down_block:
+            if isinstance(module, ResnetBlock):
+              x = module(x, context)
+              skips.append(x)
+            else:
+              x = module(x)
+        x = self.mid_block1(x, context)
+        x = self.mid_block2(x, context)
+        ind = len(skips) - 1
+        for up_block in self.ups:
+          for module in up_block:
+            if isinstance(module, ResnetBlock):
+              x = torch.cat([x, skips[ind]], dim=1)
+              ind -= 1
+              x = module(x, context)
+            else:
+              x = module(x)
         ##################################################################
 
         # Final block
